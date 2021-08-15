cd7e1bb00b5e53715a13b05d8ca57f7c26b6b85b
Shartechs <shartechs@gmail.com>
2021-08-15 16:23:10 +0400

    Added pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,10 +2,14 @@ import React, { useContext, useMemo, useState } from 'react'
 import { UserContext } from '../App'
 import User from './User'
 import TableHeader from './TableHeader'
+import TablePagination from './TablePagination'
 
 export default function Table() {
   const {searchValue, users} = useContext(UserContext)
   const [sorting, setSorting] = useState({field: "", order: ""})
+  const [totalItems, setTotalItems] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 2
 
   //defining table header values
   const headers = [
@@ -27,14 +31,17 @@ export default function Table() {
       )
       }
 
+      setTotalItems(filteredUsers.length)
+
       //Sorting users
       if(sorting.field) {
         const reversed = sorting.order === "asc" ? 1 : -1;
         filteredUsers = filteredUsers.sort((a, b) => reversed * a[sorting.field].localeCompare(b[sorting.field]))
       }
-
-    return filteredUsers;
-  }, [users, searchValue, sorting])
+    
+    //Current Page slice
+    return filteredUsers.slice((currentPage - 1) * itemsPerPage, (currentPage - 1) * itemsPerPage + itemsPerPage);
+  }, [users, currentPage, searchValue, sorting])
 
     return (
         <div className="container">
@@ -56,6 +63,15 @@ export default function Table() {
 
               </tbody>
             </table>
+            </div>
+            <div className="col-12 table--pagination-container">
+              <span>Record on page: {itemsPerPage}</span>
+              <TablePagination
+               total={totalItems}
+               itemsPerPage={itemsPerPage}
+               currentPage={currentPage}
+               onPageChange={page => setCurrentPage(page)}
+               />
             </div>
           </div>
 
